353769fb0510a8648322ef7e85f5e72bc5fce80c
Teodoro Colombo <teodorocolombo14@gmail.com>
2023-03-30 08:22:17 -0300

    Add files via upload
    Starting to port ESP32 board

---
+++ src/boards/ESP32/delay-board.c
@@ -0,0 +1,35 @@
+/*!
+ * \file      delay-board.c
+ *
+ * \brief     Target board delay implementation
+ *
+ * \copyright Revised BSD License, see section \ref LICENSE.
+ *
+ * \code
+ *                ______                              _
+ *               / _____)             _              | |
+ *              ( (____  _____ ____ _| |_ _____  ____| |__
+ *               \____ \| ___ |    (_   _) ___ |/ ___)  _ \
+ *               _____) ) ____| | | || |_| ____( (___| | | |
+ *              (______/|_____)_|_|_| \__)_____)\____)_| |_|
+ *              (C)2013-2017 Semtech
+ *
+ * \endcode
+ *
+ * \author    Miguel Luis ( Semtech )
+ *
+ * \author    Gregory Cristian ( Semtech )
+ *
+ * \author    Johannes Bruder ( STACKFORCE )
+ *
+ * \author    Marten Lootsma(TWTG) on behalf of Microchip/Atmel (c)2017
+ */
+#pragma once
+#include "delay-board.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+void DelayMsMcu(uint32_t ms)
+{
+    vTaskDelay(pdMS_TO_TICKS(ms));
+}
